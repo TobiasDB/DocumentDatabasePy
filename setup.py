9e4a94f8815076eb4d19e00e88e2b168a2d424bb
TobiasDB <tobias@dunlopbrown.com>
2021-12-22 14:49:40 +0000

    ci: add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = "0.0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+# TODO: Setup Pypi repository
+setup(
+    name="doc-db-lib",
+    description="Async Document Database Interface With ODM",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    #url="",
+    version=VERSION,
+    entry_points={
+        'console_scripts': [
+            'document_database=document_database.__init__:main',
+        ]
+    },
+    packages=find_packages(),
+    install_requires=[
+        "pydantic==1.8.2"
+    ],
+    extras_require={
+        "mongo": ["motor==2.5.1"],
+        "test": ["tox==3.24.4"]
+    },
+    tests_require=[
+        "doc-db-lib[test]"
+    ],
+    python_requires=">=3.8",
+)
